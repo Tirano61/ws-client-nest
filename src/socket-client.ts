2a55e84a42135de923bb76c56b93b031eac4ffc1
Dario Ramirez <dariorama60@gmail.com>
2024-11-23 10:38:04 -0300

    Lismpiar los listeners cunado en servidor desconecta el cliente y se vuelve a conectar

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,5 +1,8 @@
 import { Manager, Socket } from "socket.io-client";
 
+
+let socket: Socket;
+
 export const connectToServer = (token: string) =>{
     const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
         extraHeaders: {
@@ -8,13 +11,15 @@ export const connectToServer = (token: string) =>{
         }
     });
     
-    const socket = manager.socket('/');
-    
-    addListeners( socket );
+    socket?.removeAllListeners();
+
+    socket = manager.socket('/');
+
+    addListeners();
 }
 
 
-const addListeners =( socket: Socket) =>{
+const addListeners =() =>{
     const serverStatusLabel = document.querySelector('#server-status')!;
     const clientsConnected = document.querySelector('#client-ul')!;
     const mesageForm =  document.querySelector<HTMLFormElement>('#mesage-form')!;
